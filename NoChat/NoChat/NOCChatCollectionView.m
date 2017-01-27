d2ec38fa1241a83f9b8330cc91e69b89ba2514e3
little2s <duanyinglun@gmail.com>
2017-01-28 07:27:17 +0800

    fix tap

--- NoChat/NoChat/NOCChatCollectionView.m
+++ NoChat/NoChat/NOCChatCollectionView.m
@@ -8,6 +8,12 @@
 
 #import "NOCChatCollectionView.h"
 
+@interface NOCChatCollectionView ()
+
+@property (nonatomic, assign, getter=isTapState) BOOL tapState;
+
+@end
+
 @implementation NOCChatCollectionView
 
 - (instancetype)initWithFrame:(CGRect)frame collectionViewLayout:(UICollectionViewLayout *)layout
@@ -19,6 +25,7 @@
         self.showsHorizontalScrollIndicator = NO;
         self.allowsSelection = NO;
         self.scrollsToTop = NO;
+        self.alwaysBounceVertical = YES;
     }
     return self;
 }
@@ -26,8 +33,27 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [super touchesBegan:touches withEvent:event];
-    
-    if (self.tapAction) {
+    if (((UITouch *)touches.anyObject).tapCount == 1) {
+        self.tapState = YES;
+    }
+}
+
+- (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesMoved:touches withEvent:event];
+    self.tapState = NO;
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesCancelled:touches withEvent:event];
+    self.tapState = NO;
+}
+
+- (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [super touchesEnded:touches withEvent:event];
+    if (self.tapAction && self.isTapState) {
         self.tapAction();
     }
 }
